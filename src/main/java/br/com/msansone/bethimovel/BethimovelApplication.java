3acf3e505a06166d884b555c8fe92be993fd710a
marcio sansone <msansone@gmail.com>
2022-12-25 14:24:11 +0000

    First commit - just spring initializ

---
+++ src/main/java/br/com/msansone/bethimovel/BethimovelApplication.java
@@ -0,0 +1,13 @@
+package br.com.msansone.bethimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethimovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethimovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msansone/bethimovel/BethimovelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msansone.bethimovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethimovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
